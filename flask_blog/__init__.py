09b93b10f69f504d3e9f25635656ed45550c42ee
yongwoon <yonyon.japan@gmail.com>
2020-07-19 00:19:26 +0900

    Create POST

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 # Flask application 全体を作成
 app = Flask(__name__)
 
 # config file の有効化
 app.config.from_object('flask_blog.config')
+
+db = SQLAlchemy(app)
 
 from flask_blog.views import views, entries
