8f34cea0baf54d22b694c14e2c9b22b717719090
yongwoon <yonyon.japan@gmail.com>
2020-07-18 23:57:26 +0900

    View を切り出す

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 # Flask application 全体を作成
 app = Flask(__name__)
@@ -7,6 +6,4 @@ app = Flask(__name__)
 # config file の有効化
 app.config.from_object('flask_blog.config')
 
-db = SQLAlchemy(app)
-
-import flask_blog.views
+from flask_blog.views import views, entries
