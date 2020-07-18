2097ba8ca7377a1a4179db8dd1c1e56514b1a9b5
yongwoon <yonyon.japan@gmail.com>
2020-07-18 23:17:37 +0900

    Setting flask_sqlalchemy

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Flask application 全体を作成
 app = Flask(__name__)
 
 # config file の有効化
 app.config.from_object('flask_blog.config')
+
+db = SQLAlchemy(app)
 
 import flask_blog.views
--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,3 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 SECRET_KEY = 'secret key'  # seesion 情報を暗号化
 USERNAME = 'yongwoon'
