ad4869532c7f12091baa90b9be33ea0a8a76679d
yongwoon <yonyon.japan@gmail.com>
2020-07-18 13:45:28 +0900

    set config variable

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -12,7 +12,7 @@ def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             print('ユーザー名が異なります')
-        elif reuqest.form['password'] != app.config['PASSWORD']:
+        elif request.form['password'] != app.config['PASSWORD']:
             print('パスワードが異なります')
         else:
             return redirect('/')
