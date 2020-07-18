b069352d764e8d765bfd62d5d39f4b510d939bf3
yongwoon <yonyon.japan@gmail.com>
2020-07-19 01:46:50 +0900

    Blueprint による views view の分割

--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -1,18 +1,21 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from functools import wraps
+from flask import Blueprint
+
+view = Blueprint('view', __name__)
 
 
 def login_required(view):
     @wraps(view)
     def inner(*args, **kwargs):
         if not session.get('logged_in'):
-            return redirect(url_for('login'))
+            return redirect(url_for('view.login'))
         return view(*args, **kwargs)
     return inner
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@view.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -23,12 +26,21 @@ def login():
         else:
             session['logged_in'] = True
             flash('ログインしました')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('entry.show_entries'))
     return render_template('login.html')
 
 
-@app.route('/logout')
+@view.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('ログアウトしました')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('entry.show_entries'))
+
+
+# root url に紐づく処理がなくなったので、追加
+#
+# errorhandler method ですが、blueprint application を利用した場合、
+# app_errorhandler を使う必要がありまうsので変更
+@view.app_errorhandler(404)
+def non_existant_route(error):
+    return redirect(url_for('view.login'))
